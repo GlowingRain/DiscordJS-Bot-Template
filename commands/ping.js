cf25c62613aca689c6cc6f1bdf5b621048c1ee8d
GlowingRain <44713297+GlowingRain@users.noreply.github.com>
2019-02-10 17:01:43 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -10,7 +10,7 @@ module.exports = {
     execute(client, message, args) {
         let msgping1 = new Date();
         let botping = new Date() - message.createdAt;
-		let msgping2 = new Date() - msgping1;
+	let msgping2 = new Date() - msgping1;
 
         message.channel.send(`Calculating!`).then(msg => {
             msg.edit(`❤️ ${Math.round(botping)}ms\n⌚️ ~${Math.round(msgping2)}ms`)
