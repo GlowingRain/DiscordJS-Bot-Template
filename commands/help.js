f295a625ec5fbaa595ac9390582506b70e2f12c7
xem <xem.discord.gg@gmail.com>
2019-02-14 05:35:11 -0300

    fixed styling

--- commands/help.js
+++ commands/help.js
@@ -15,14 +15,14 @@ module.exports = {
             .setColor("BLUE");
 
         if (!args.length) {
-            embed.setAuthor("Help command!", client.user.displayAvatarURL)
+            embed.setAuthor("Help command!", client.user.displayAvatarURL);
             embed.addField("Information", `${tools.MapCategories(client, "Information")}`, true);
             // Add more fields if there are more categories.
 
             embed.setFooter(`You can do ${prefix}help [command] to see aditional info!`);
             return message.channel.send(embed);
         } else {
-            // Show the command's info. 
+            // Show the command's info.
             const name = args[0].toLowerCase();
             const command = client.commands.get(name) || client.commands.find(c => c.aliases && c.aliases.includes(name));
 
